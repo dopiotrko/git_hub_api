de088c82e8fda21d1995299ac9745adea36ef85b
piotrkowalcp <piotrkowalczyk@housetipster.com>
2024-08-09 22:37:12 +0100

    authentication upgrade

--- src/main/java/GitHubRestapi/controller/GitHubHeader.java
+++ src/main/java/GitHubRestapi/controller/GitHubHeader.java
@@ -6,7 +6,6 @@ public class GitHubHeader {
 
 
     public static HttpHeaders getUserAgentHeader(String token) {
-//        String token = "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";
         HttpHeaders headers = new HttpHeaders();
         headers.add("User-Agent", "dopiotrko");
         headers.add("Accept", "application/json");
